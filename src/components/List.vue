e94d137d14bc22e1146e5b2daee3aeb1d8ad0dec
Pedro <hernansaldana52@gmail.com>
2020-06-20 20:44:38 -0500

    Empezando las pruebas fetch para trabajar con json

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
+
     <div class= "box" v-for="e in elements" v-bind:key="e">
       {{e}}
   </div>
+
+  <button v-on:click="findAll">Click</button>
+
+  {{msj}}
+  
   </div>
 </template>
 
@@ -10,9 +16,17 @@
 export default {
   data(){
     return {
+      msj: "..... :(",
       elements: ["Uno","Dos","Tres"]
     };
-  }
+  },
+  methods: {
+    findAll: function(){
+      fetch('http://127.0.0.1:8000/api/element/?format=json')
+      .then(res => res.json)
+      .then(res => console.log(res))
+    }
+  },
 }
 </script>
 
